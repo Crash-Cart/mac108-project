47d2e98d821f143457f1da680eb5f859fce1e44d
mugglemedic <65924889+mugglemedic@users.noreply.github.com>
2026-04-15 12:33:56 -0400

    readme, todo in parser docstring, formatting changes/plans

--- README.md
+++ README.md
@@ -1,24 +1,32 @@
-# mac108-project
+# <h1 align="center"> MAC 108 Project</h1>
 My project for Intro to Python (MAC 108) at LaG CC
 
-![Neovim](https://img.shields.io/badge/Editor-Neovim-57A143?logo=neovim)  
-![Python](https://img.shields.io/badge/Python-3.14%2B-blue?logo=python)  
+
+![Neovim](https://img.shields.io/badge/Editor-Neovim-57A143?logo=neovim)
+![Python](https://img.shields.io/badge/Python-3.14%2B-blue?logo=python) 
 ![License](https://img.shields.io/badge/license-_MIT_-fb542b)
+![Static Badge](https://img.shields.io/badge/BTW-Arch?style=plastic&logo=archlinux&logoColor=%231793D1&labelColor=black&color=%231793D1)
+
 
 ## Planning  
+
 
 ### Scripting  
 
 - Create a file to parse auth logs for failed attempts and identify patterns
+
     - detect abnormal hours
     - flag unknown IPs
     - ID signs of brute force attack
     - IF creating a cloud instance -- EC2 ubuntu machine -- logs located /var/log/ 
+
 - Call an alerting script for suspiscious activity listed above
     - email/text notification blast
         - find a service if possible
-    - print in montioring terminal
-- Summaru 
+
+- Print incoming alerts in an open montioring terminal
+
+- Summary generator
     - Reporting of events
     - show trends of repeat IP offenders
     - matplotlib to graph trends for presentation
@@ -34,12 +42,15 @@ My project for Intro to Python (MAC 108) at LaG CC
 
 ### Network/Attack Surface
 - Options:
+
     - Set up a honeypot on azure or aws
         - Pros: Can set up a true SIEM to compare outcomes 
         - Cons: Might get expensive if not monitored closely
+    
     - Create a VLAN on home network and simulate attacks
         - Easier to implement
         - No "real" hands-on experience
+    
     - Use my existing web domain in some way?
         - No idea how to go about this, will have to consult a lot of documentation. 
 
--- logparser.py
+++ logparser.py
@@ -4,10 +4,20 @@ Intro to Python Project
 This is a script will open and parse access log files and consolidate data. 
 The data will be used to identify patterns in login attempts.
 Author: NAME_HERE, LaGuardia Community College, MAC 108 sec.3033
-Sources: claude.ai, https://www.w3schools.com/python/python_regex.asp , https://docs.python.org/3/library/re.html
+Sources: claude.ai, https://www.w3schools.com/python/python_regex.asp , https://docs.python.org/3/library/re.html , https://www.w3schools.com/python/python_datetime.asp 
 """
 
+
+'''
+TODO:
+     - def then def main and call each as needed, move top logic to main
+     - Change name before submitting
+     - count fails and IPS  -- format into dictionary(?)
+     - pull/merge desktop to sync repo
+'''
+
 import re
+from datetime import datetime
 
 listed_fails = []
 with open('sample_auth.log') as log:  # CHANGE to '/var/log/auth.log' before deployment
@@ -18,9 +28,18 @@ with open('sample_auth.log') as log:  # CHANGE to '/var/log/auth.log' before dep
         # Parse file for failed login attempts and add to a list.
 
 
+def parse_time(line):
+    time_string = ' '.join(line.split()[:3])
+    return datetime.strptime(time_string, "%b %d %H:%M:%S")  #noqa
+     
+
+
+# make this a function and call in larger func, maybe class creation?
+def parse_ip():
+    pass
 ip_a = []
 for fail in listed_fails:
-    ip_a.append(re.findall(r'(\b(?:\d{1,3}\.){3}\d{1,3}\b)', fail, re.X))
+    ip_a.append(re.findall(r'(\b(?:\d{1,3}\.){3}\d{1,3}\b)', fail, re.X))  #noqa
     # Add IPs to a list
 
     '''I had to ask claude for help, specifying not to give the answer:
@@ -37,8 +56,8 @@ for fail in listed_fails:
         So you need four of those groups with dots between them. 
         Give it another shot with that in mind — how would you chain those pieces together?'''
 
-
-''' count fails and IPS  -- format into dictionary(?) '''
+def main():
+    pass
 
 if __name__ == "__main__":
     # print fail logs
@@ -48,3 +67,4 @@ if __name__ == "__main__":
     print('Logged IPs')
     # check list of IP addresses
     print(ip_a)
+    main() 
